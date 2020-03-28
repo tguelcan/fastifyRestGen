3d76e98a429ba7bdb133c8e6228f5e8a874c1dcb
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-03-28 18:10:44 +0100

    added try-catch error handling

--- src/api/messages/controller.js
+++ src/api/messages/controller.js
@@ -2,33 +2,53 @@ import { merge } from 'lodash'
 import Message from './model'
 
 export const index = async ({ query: { filter, skip, limit, sort, projection, population } }, res) => {
-    const messages = await Message.find(filter)
-        .skip(skip)
-        .limit(limit)
-        .sort(sort)
-        .select(projection)
-        .populate(population)
-    res.send(messages.map((message) => message.view()))
+    try {
+        const messages = await Message.find(filter)
+            .skip(skip)
+            .limit(limit)
+            .sort(sort)
+            .select(projection)
+            .populate(population)
+        res.send(messages.map((message) => message.view()))        
+    } catch (error) {
+        res.badRequest(error)
+    }
 }
 
 export const show = async ({ params: { id } }, res) => {
-    const message = await Message.findById(id)
-    message ? res.send(message.view()) : res.notFound()
+    try {
+        const message = await Message.findById(id)
+        message ? res.send(message.view()) : res.notFound()    
+    } catch (error) {
+        res.badRequest(error)
+    }
 }
 
 export const create = async ({ body }, res) => {
-    const message = await Message.create(body)
-    res.send(message.view())
+    try {
+        const message = await Message.create(body)
+        res.send(message.view())            
+    } catch (error) {
+        res.badRequest(error)
+    }
 }
 
 export const update = async ({ body, params: { id } }, res) => {
-    const message = await Message.findById(id)
-    message ? merge(message, body).save() : res.notFound()
-    res.send(message.view())
+    try {
+        const message = await Message.findById(id)
+        message ? merge(message, body).save() : res.notFound()
+        res.send(message.view())            
+    } catch (error) {
+        res.badRequest(error)
+    }
 }
 
 export const destroy = async ({ params: { id }  }, res) => {
-    const message = await Message.findById(id)
-    message ? message.remove() : res.notFound()
-    res.send('success')
+    try {
+        const message = await Message.findById(id)
+        message ? message.remove() : res.notFound()
+        res.send('success')    
+    } catch (error) {
+        res.badRequest(error)
+    }
 }

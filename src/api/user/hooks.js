082c99ffdd950181a2c3df8bf75a95419895e3c7
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-03-31 18:42:20 +0200

    Check if body exists

--- src/api/user/hooks.js
+++ src/api/user/hooks.js
@@ -2,7 +2,7 @@ import { hashPassword } from '~/utils'
 
 export default (app) => {
     app.addHook('preHandler', async (request, reply) => {
-        if (request.body.password !== undefined) {
+        if (request.body?.password !== undefined) {
             request.body.password = await hashPassword(request.body.password)
         }
         return
